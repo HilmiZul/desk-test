f99c5f3ec0ce23d013367af6511215fa60ffec8b
hilmizul <netspytux@gmail.com>
2019-06-16 21:45:41 +0700

    Initial Commit

---
+++ Desk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Desk
+//
+//  Created by Zul Hilmi on 16/06/19.
+//  Copyright © 2019 Zul Hilmi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Desk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Desk
+//
+//  Created by Zul Hilmi on 16/06/19.
+//  Copyright © 2019 Zul Hilmi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
